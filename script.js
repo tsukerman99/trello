cd012f4fe0b17bbb1c35e53d06c65896f18eb24c
tsukerman <tsukerman99@gmail.com>
2018-08-18 13:35:44 +0300

    style

--- script.js
+++ script.js
@@ -6,5 +6,29 @@
 		defaultCard.className = 'card';
 		var lastChild = group.children[group.children.length - 1];
 		group.insertBefore(defaultCard, lastChild);
+
+		
+		defaultCard.innerHTML = 
+		'<div class="progress-bar">'+
+			'<progress value="60" max="100"></progress>' +
+			'<i id="clear" class="material-icons">clear</i>' +
+		'</div>' +
+		'<div class="title">' +
+			'<p>Title 1</p>' + 
+		'</div>' +
+		'<div class="button">' +
+			'<button class="more">' +
+				'<i class="material-icons">more_horiz</i>' +
+			'</button>' +
+			'<div class="circle"></div>' +
+			new Date().toDateString() +
+			'<img class="img" src="images/img.jpg" >' +
+		'</div>';
+
+		function removeElement(card) {
+			// Removes an element from the document
+			var element = document.getElementById(elementId);
+			element.parentNode.removeChild(element);
+		}
 	};
 })();
